b3aa648f06770a1e7db1caea13fbe51d48f5574a
minhduc3008 <vuminhduc3008@gmail.com>
2023-05-19 15:29:53 +0700

    update switch case

---
+++ bai9/js/exam2.js
@@ -0,0 +1,31 @@
+//Switch case
+
+let today = 1;
+
+switch(today) {
+    case 1: 
+        console.log('ko có ngày thứ 1');
+        break;
+    case 2: 
+        console.log('Hôm nay là thứ Hai');
+        break;
+    case 3: 
+        console.log('Hôm nay là thứ Ba');
+        break;
+    case 4: 
+        console.log('Hôm nay là thứ Tư');
+        break;
+    case 5: 
+        console.log('Hôm nay là thứ Năm');
+        break;
+    case 6: 
+        console.log('Hôm nay là thứ Sáu');
+        break;
+    case 7: 
+        console.log('Hôm nay là thứ Bảy');
+        break;
+    default:
+        console.log('Hôm nay là chủ nhật');
+        break;
+    
+}
