daca003c5de11f1394574d8e9cc66a3bbd069a51
minhduc3008 <vuminhduc3008@gmail.com>
2023-05-26 16:48:48 +0700

    format code

--- bai12/js/exam.js
+++ bai12/js/exam.js
@@ -24,29 +24,29 @@ document.write("<br>");
 document.write("Trường đại học: " + student.university.name);
 
 // Bài tập 1
-let user = {}  // tạo ra object rỗng
-user.name = 'John' // thêm name = John
-user.surname = 'Parker' // thêm surname = Smith
-user.name = 'Peter' // thay đổi name = Pete
-user.fullname = function() {
-    return this.name + ' '+ this.surname
-}
-console.log(user.name)
-console.log(user.fullname())
+let user = {}; // tạo ra object rỗng
+user.name = "John"; // thêm name = John
+user.surname = "Parker"; // thêm surname = Smith
+user.name = "Peter"; // thay đổi name = Pete
+user.fullname = function () {
+  return this.name + " " + this.surname;
+};
+console.log(user.name);
+console.log(user.fullname());
 // Bài tập 2
 
 const calculator = {
-    read: function(so1, so2) {
-      this.so1 = so1;
-      this.so2 = so2;
-    },
-    sum: function() {
-      return this.so1 + this.so2;
-    },
-    avg: function() {
-      return (this.so1 + this.so2) / 2;
-    }
-  };
+  read: function (so1, so2) {
+    this.so1 = so1;
+    this.so2 = so2;
+  },
+  sum: function () {
+    return this.so1 + this.so2;
+  },
+  avg: function () {
+    return (this.so1 + this.so2) / 2;
+  },
+};
 
 calculator.read(1, 3);
 console.log(calculator.sum());
