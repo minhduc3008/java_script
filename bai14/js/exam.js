566dc87e6b9a0075ee6b3e619cbba226ca0f9ef6
minhduc3008 <vuminhduc3008@gmail.com>
2023-05-29 10:53:24 +0700

    bai14 done

---
+++ bai14/js/exam.js
@@ -0,0 +1,20 @@
+/*
+    Array
+
+    Cách khai báo: 
+
+        tenArray = [phantu1, phantu2, phantu3, ...]
+        tenArray = new Array(phantu1, phantu2, phantu3, ...)
+
+    Cách thêm phần tử mới cho mảng:
+
+        tenArray.push(phantuMoi)
+
+ */
+
+let students = new Array(1, 3, 4);
+let trum = ['Đức', 'Trump', 'Biden', 'Obama']
+students.push('Minh Đức');
+trum.push('Washington');
+console.log(students[3]);
+console.log(trum[1])
